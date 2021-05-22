348bd1993e1bdb518b5a1bf098421386ad23eed6
Aidana B <dana12021996@gmail.com>
2021-05-22 11:54:23 -0400

    added more hello features

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("Developing hello feature");
-        //enhancing hello
+        System.out.println("Local repo already published to GitHUb");
+        System.out.println("Doing more work");
     }
 }
